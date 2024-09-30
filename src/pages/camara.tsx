5a8469bd64899b9b30ec0f47fb350f04e1b991a9
Travis Nong <277096128@qq.com>
2024-09-30 21:05:12 +1300

    feat(upload): change route

---
+++ src/pages/camara.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Camara = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Camara</h1>
+            <p>This is the camara page.</p>
+        </div>
+    );
+};
+
+export default Camara;
