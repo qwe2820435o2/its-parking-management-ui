b144c6fc2270daeebda6269fe32144df94053c37
Travis Nong <277096128@qq.com>
2024-09-30 18:52:22 +1300

    feat(css): add shadcn-ui button dialog

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,36 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	'./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#2563EB',    // 主色调 (蓝色)
-        secondary: '#F3F4F6',  // 副色调 (浅灰)
-        accent: '#10B981',     // 强调色 (绿色)
-        warning: '#F59E0B',    // 警告色 (黄色)
-        danger: '#EF4444',     // 错误色 (红色)
-        textPrimary: '#1F2937',  // 文本颜色 (深灰)
-        textSecondary: '#9CA3AF', // 次要文本颜色 (浅灰)
-        darkBg: '#111827',       // 深色模式背景
-        darkContentBg: '#1F2937', // 深色模式内容背景
-        lightText: '#F9FAFB',    // 深色模式文本
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#2563EB',
+  			secondary: '#F3F4F6',
+  			accent: '#10B981',
+  			warning: '#F59E0B',
+  			danger: '#EF4444',
+  			textPrimary: '#1F2937',
+  			textSecondary: '#9CA3AF',
+  			darkBg: '#111827',
+  			darkContentBg: '#1F2937',
+  			lightText: '#F9FAFB',
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
